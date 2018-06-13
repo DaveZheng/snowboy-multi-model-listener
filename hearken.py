d22507fd4278f36e8c839a8347998501a248db8e
David Zheng <dzheng@viewpointcloud.com>
2018-06-13 16:11:27 -0400

    completing module structure, figuring out environments structure, finishing execution on main app

--- hearken.py
+++ hearken.py
@@ -2,6 +2,7 @@ import ConfigParser, os, importlib
 import sys
 import signal
 
+from common import snowboydecoder
 
 #################################
 # Check for interrupt signal
@@ -28,6 +29,7 @@ signal.signal(signal.SIGINT, signal_handler)
 
 DEFAULT_MODULE = 'basic_ding'
 CONF_FILE_NAME = 'hearken.conf'
+ENV = 'demo'
 
 # Count of hotwords said in short amount of time before firing action
 HOTWORD_COUNT_THRESHHOLD = 2
@@ -38,7 +40,6 @@ SELECTED_MODULE = DEFAULT_MODULE
 config = ConfigParser.SafeConfigParser()
 
 # TODO: Allow for overwrite of conf file to use, with argparser
-# TODO: Allow for sensitivity setting
 
 # Create .conf file with defaults if it doesn't exist
 if not os.path.exists(CONF_FILE_NAME):
@@ -46,6 +47,7 @@ if not os.path.exists(CONF_FILE_NAME):
 	config.add_section('APP_RUNTIME')
 	config.set('APP_RUNTIME', 'SELECTED_MODULE', SELECTED_MODULE)
 	config.set('APP_RUNTIME', 'HOTWORD_COUNT_THRESHHOLD', '2')
+	config.set('APP_RUNTIME', 'ENV', ENV)
 	config.write(open(CONF_FILE_NAME, 'w'))
 
 # Read existing conf and load the module
@@ -53,6 +55,7 @@ else:
 	config.read(CONF_FILE_NAME)
 	HOTWORD_COUNT_THRESHHOLD = config.getint('APP_RUNTIME', 'HOTWORD_COUNT_THRESHHOLD')
 	SELECTED_MODULE = config.get('APP_RUNTIME', 'SELECTED_MODULE')
+	ENV = config.get('APP_RUNTIME', 'ENV')
 
 mod_import = 'modules.' + SELECTED_MODULE + '.' + SELECTED_MODULE
 
@@ -66,6 +69,26 @@ except ImportError:
 
 print "HOTWORD_COUNT_THRESHHOLD: ", HOTWORD_COUNT_THRESHHOLD
 print "LOADED MODULE: " + SELECTED_MODULE
+print "ENVIRONMENT: " + ENV
 
-# TODO: Run SETUP and LISTEN commands to start the app
+# TODO: SETUP callbacks and LISTEN commands to start the app
 
+models = mod.loadModels(ENV)
+callbacks = mod.loadCallbacks(models)
+
+
+# TODO: Allow for sensitivity setting
+
+sensitivity = [0.5]*len(models)
+
+# Arm the hotword detector
+detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
+
+print 'Listening... Press Ctrl+C to exit'
+
+# main loop
+detector.start(detected_callback=callbacks,
+               interrupt_check=interrupt_callback,
+               sleep_time=0.03)
+
+detector.terminate()
--- modules/basic_ding/basic_ding.py
+++ modules/basic_ding/basic_ding.py
@@ -1,5 +1,41 @@
-import os, os.path 
+import os, os.path
+import sys
 
 from common import snowboydecoder
 
+MODULE_PATH = os.path.dirname(__file__)
+
+# TODO: extend an abstract class with loadModels and loadCallbacks
+
 # TODO: Setup and Run commands 
+
+def loadModels(pEnv):
+
+	try:
+		env_dir = os.path.join(MODULE_PATH, pEnv)
+
+		print 'Attempting to load models from directory: ', env_dir
+
+		env_dir_files = os.listdir(env_dir)
+		model_files = [f for f in env_dir_files if (f.endswith('.umdl') or f.endswith('.pmdl'))]
+
+		if len(model_files) == 0:
+				print("Error: module and environment combination needs at least 1 associated model! (.umdl, .pmdl).")
+				print("There are no model files in ", env_dir)
+				sys.exit(-1)
+		else: 
+				print "Loading {0} hotword files: ".format(len(model_files))
+				print model_files
+			
+	except OSError:
+			print "No such directory: ", env_dir
+			sys.exit(-1)
+	except:
+			print "Unexpected error:", sys.exc_info()[0]
+	
+	return [os.path.join(env_dir, f) for f in model_files]
+
+
+def loadCallbacks(pModels):
+
+	return [lambda: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING) for model in pModels]
