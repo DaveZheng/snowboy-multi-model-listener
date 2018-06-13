e53fffa6daf964a05c86c4df09b3c849bbff5882
David Zheng <dzheng@viewpointcloud.com>
2018-06-12 23:59:20 -0400

    hotword count threshhold

--- hearken.py
+++ hearken.py
@@ -1,25 +1,30 @@
 import ConfigParser, os, importlib
-# import modules.basic_ding.basic_ding
 
 DEFAULT_MODULE = 'basic_ding'
 CONF_FILE_NAME = 'hearken.conf'
 
+# Count of hotwords said in short amount of time before firing action
+HOTWORD_COUNT_THRESHHOLD = 2
+
+# Selected module
 SELECTED_MODULE = DEFAULT_MODULE
 
 config = ConfigParser.SafeConfigParser()
 
 # TODO: Allow for overwrite of conf file to use, with argparser
 
-# Create .conf file if it doesn't exist
+# Create .conf file with defaults if it doesn't exist
 if not os.path.exists(CONF_FILE_NAME):
 	print ".conf file does not exist. generating default conf"
 	config.add_section('APP_RUNTIME')
 	config.set('APP_RUNTIME', 'SELECTED_MODULE', SELECTED_MODULE)
+	config.set('APP_RUNTIME', 'HOTWORD_COUNT_THRESHHOLD', '2')
 	config.write(open(CONF_FILE_NAME, 'w'))
 
 # Read existing conf and load the module
 else:
 	config.read(CONF_FILE_NAME)
+	HOTWORD_COUNT_THRESHHOLD = config.getint('APP_RUNTIME', 'HOTWORD_COUNT_THRESHHOLD')
 	SELECTED_MODULE = config.get('APP_RUNTIME', 'SELECTED_MODULE')
 
 mod_import = 'modules.' + SELECTED_MODULE + '.' + SELECTED_MODULE
@@ -32,6 +37,7 @@ except ImportError:
 	print "MODULE: " + SELECTED_MODULE + " does not exist. Please specify valid module."
 	quit()
 
+print "HOTWORD_COUNT_THRESHHOLD: ",HOTWORD_COUNT_THRESHHOLD
 print "LOADED MODULE: " + SELECTED_MODULE
 
 # TODO: Run SETUP and LISTEN commands to start the app
